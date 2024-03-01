0a028980ac8265c19dfb7d1c7507d20348af294f
chiii <chei94117@gmail.com>
2024-03-01 16:23:15 +0800

    引入tailwind & 基本設置

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: [],
+	theme: {
+		colors: {
+			primary: {
+				DEFAULT: '#E6EBE9',
+				dark: '#747D78',
+			},
+		},
+	},
+	plugins: [],
+};

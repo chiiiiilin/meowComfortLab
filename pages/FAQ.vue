0e12f41e5c9cf156abbd7777cf332d3588c4d021
chiii <chei94117@gmail.com>
2024-03-21 17:44:50 +0800

    feat: 調整版面css

--- pages/FAQ.vue
+++ pages/FAQ.vue
@@ -10,14 +10,10 @@
 				<div
 					v-for="content in doc.contents"
 					:key="content.id"
-					class="py-3 lg:w-[45%] box-border"
+					class="lg:w-[45%] box-border"
 				>
 					<h3>{{ content.title }}</h3>
-					<div
-						v-for="detail in content.details"
-						:key="detail.id"
-						class="py-2"
-					>
+					<div v-for="detail in content.details" :key="detail.id">
 						<h4 class="text-lg">{{ detail.title }} :</h4>
 						<p class="text-gray-400">
 							{{ detail.description }}
@@ -26,16 +22,16 @@
 				</div>
 			</div>
 		</section>
-		<section>
+		<section class="px-4 py-5">
 			<h2>訂製申請表單</h2>
-			<h3 class="py-3">
+			<h3>
 				如需客製化商品，或室內空間規劃服務，請填寫此表單，將由專人聯繫為您服務。
 			</h3>
 		</section>
 	</main>
 </template>
 
-<script setup>
+<script setup lang="ts">
 const mainStore = useMainStore();
 
 onMounted(async () => {
@@ -45,11 +41,4 @@ onMounted(async () => {
 });
 </script>
 
-<style scoped>
-h2 {
-	@apply text-3xl px-3 lg:px-5 border-l-4 border-l-accent translate-x-[-0.75rem] lg:translate-x-[-1.6rem];
-}
-h3 {
-	@apply text-accent font-semibold tracking-wide;
-}
-</style>
+<style scoped src="@/assets/css/normalStyle.css"></style>
